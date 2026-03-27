5df62431f45f828b2c7fd284edc6380689e8cbe4
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-03-27 19:14:01 +0300

    images changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Прайм",
+  title: "Прайм - Строительная компания",
   description: "Строительная компания",
 };
 
