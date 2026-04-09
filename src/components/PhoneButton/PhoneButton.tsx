abd90a2098fb4dacd73c9df1bc7267286691d434
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-04-09 19:23:37 +0300

    phone anim

--- src/components/PhoneButton/PhoneButton.tsx
+++ src/components/PhoneButton/PhoneButton.tsx
@@ -6,7 +6,7 @@ export default function PhoneButton() {
     return (
         <button className={styles.button}>
             <Link className={styles.link} href={"tel:+79882233333"} target="_blank">
-                <Image src="/images/icons/phone.svg" alt='телефон' width={32} height={32}></Image>
+                <Image src="/images/icons/phone.svg" alt='телефон' width={32} height={32} className={styles.icon}></Image>
             </Link>
         </button>
     )
