d178eecf782fbf579904bfb33eb79bf0d1cb8bbf
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-04-18 17:20:02 +0300

    whatsup link

--- src/sections/PresentationDownload/PresentationDownload.tsx
+++ src/sections/PresentationDownload/PresentationDownload.tsx
@@ -4,14 +4,18 @@ import LineUp from "../../components/DecorLines/LineUp"
 import LineDown from "../../components/DecorLines/LineDown"
 
 export default function PresentationDownloadSection() {
+
+    const phone = "89882233333";
+    const message = "Получить планировки на ЖК ПРИТЯЖЕНИЕ";
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
     return (
         <div className={styles.container}>
             <div className={styles.title}>Скачайте презентацию<br />ЖК Притяжение</div>
             <div className={styles.description}>
                 Подробная информация о комплексе, технические характеристики, инфраструктура района и условия рассрочки в одном PDF-файле.
             </div>
-            <Link href={'/files/Prityazheniye.pdf'} download>
-                <button className={styles.button} >Скачать <span>(35.3 МБайт)</span></button>
+            <Link href={url} target="_blank" rel="noopener noreferrer">
+                <button className={styles.button} >Получить презентацию</button>
             </Link>
         </div>
     )
