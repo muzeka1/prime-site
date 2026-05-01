5f9167d7371da5963159d44ce8edea30a8d99a5f
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-05-01 16:46:44 +0300

    scrollTo problems

--- src/sections/Header/header.tsx
+++ src/sections/Header/header.tsx
@@ -27,9 +27,7 @@ export default function Header() {
 
     if (el && lenis) {
       lenis.scrollTo(el, {
-        offset: -80,
         duration: 1.2,
-        immediate: false,
       });
     }
   };
