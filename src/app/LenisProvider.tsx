f7ba7e6e225dee655daf2b8b612cbd389499b49c
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-05-01 17:08:21 +0300

    scrollTo problems

--- src/app/LenisProvider.tsx
+++ src/app/LenisProvider.tsx
@@ -10,6 +10,9 @@ export default function LenisProvider({ children }: { children: ReactNode }) {
       options={{
         lerp: 0.05,
         smoothWheel: true,
+        syncTouch: true,
+        touchMultiplier: 35,
+        duration: 1.2,
       }}
     >
       {children}
