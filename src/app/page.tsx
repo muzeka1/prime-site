6c3070d64943d4caec2baea35c15db87be1ec4b6
Abdulla Khalitov <abdullahalitoff@gmail.com>
2026-05-01 16:18:08 +0300

    apartments, gallery blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
         <PhoneButton/>
         <ScrollToTopButton/>
         <section>
-          <Header aboutSKSectionRef={aboutSKSectionRef}></Header></section>
+          <Header></Header></section>
         <section>
           <GeneralInfoSection/>
           <MainInfoSection/>
